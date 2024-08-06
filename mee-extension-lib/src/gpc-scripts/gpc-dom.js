1c92678a0a3d0dd41375ecb3eed5a88586787916
Raman Siliuk <rsiluk@swiftinvention.com>
2024-08-06 12:50:26 +0400

    added use createScript for Chrome

--- mee-extension-lib/src/gpc-scripts/gpc-dom.js
+++ mee-extension-lib/src/gpc-scripts/gpc-dom.js
@@ -9,7 +9,21 @@ function setDomSignal() {
       document.currentScript.parentElement.removeChild(document.currentScript);
     `;
     const script = document.createElement("script");
-    script.innerHTML = script_content;
+    if (window.trustedTypes && window.trustedTypes.createPolicy) {
+      const escapeHTMLPolicy = window.trustedTypes.createPolicy("default", {
+        createHTML: (string) =>
+          string
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&apos;"),
+        createScript: (string) => string,
+      });
+      const escaped = escapeHTMLPolicy.createScript(script_content);
+      script.innerHTML = escaped;
+    } else {
+      script.innerHTML = script_content;
+    }
     document.documentElement.prepend(script);
   } catch (err) {
     console.error(`Failed to set DOM signal: ${err}`);
