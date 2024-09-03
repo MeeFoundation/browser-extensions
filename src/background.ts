1a6611025f0248f85d9ce8828ddf933e942c61a6
Gilad <gk2610@columbia.edu>
2024-09-03 17:23:45 -0400

    + prettier, bert, & reorg

--- src/background.ts
+++ src/background.ts
@@ -9,104 +9,114 @@ import {
   enableNavigatorGPC,
   disableNavigatorGPC,
   getUserInfo,
-  addUserInfo,
-} from "mee-extension-lib";
+  addUserInfo
+} from 'mee-extension-lib'
 
-import config from "./config";
-import { getSDAData, parseTaxonomyRecords } from "./sda-profile";
+import config from './config'
+import { getSDAData, parseTaxonomyRecords } from './sda-profile'
 
-initDB();
+initDB()
 
-function afterDownloadWellknown(
-  message: Message,
-  sender: chrome.runtime.MessageSender
-) {
-  let tabID = sender.tab?.id;
-  const url = sender.url;
+function afterDownloadWellknown(message: Message, sender: chrome.runtime.MessageSender) {
+  let tabID = sender.tab?.id
+  const url = sender.url
 
   if (url && tabID) {
-    let domain = getDomainFromUrl(url);
-    let wellknown = [];
+    let domain = getDomainFromUrl(url)
+    let wellknown = []
 
-    wellknown[tabID] = message.data;
-    let wellknownData = message.data;
+    wellknown[tabID] = message.data
+    let wellknownData = message.data
 
-    const gpc =
-      wellknown[tabID] && wellknown[tabID]["gpc"] === true ? true : false;
+    const gpc = wellknown[tabID] && wellknown[tabID]['gpc'] === true ? true : false
 
     addRowToDB({
       domain: domain,
       wellknown: gpc,
-      enabled: true,
-    });
+      enabled: true
+    })
 
     chrome.runtime.onMessage.addListener((message) => {
-      if (message.msg === "POPUP_LOADED") {
+      if (message.msg === 'POPUP_LOADED') {
         chrome.runtime.sendMessage({
-          msg: "SEND_WELLKNOWN_TO_POPUP",
-          data: { domain, wellknownData },
-        });
+          msg: 'SEND_WELLKNOWN_TO_POPUP',
+          data: { domain, wellknownData }
+        })
       }
-    });
+    })
   }
 }
 
-let taxonomyRecords: string[][];
+let taxonomyRecords: string[][]
 
 const getTaxonomyRecords = async () => {
-  const response = await fetch(chrome.runtime.getURL("/taxonomy.tsv"));
-  const taxonomy = await response.text();
-  taxonomyRecords = parseTaxonomyRecords(taxonomy);
-};
+  const response = await fetch(chrome.runtime.getURL('/taxonomy.tsv'))
+  const taxonomy = await response.text()
+  taxonomyRecords = parseTaxonomyRecords(taxonomy)
+}
 
-getTaxonomyRecords();
+getTaxonomyRecords()
 
 const onInitPage = async () => {
-  const userInfo = await getUserInfo();
-  const userUid = userInfo?.user_uid ? userInfo.user_uid : crypto.randomUUID();
-  if (!userInfo.user_uid) {
-    addUserInfo(userUid);
-  }
-  const data = await getSDAData(taxonomyRecords, userUid);
-  try {
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    const request = new Request(config.backendUrl + "api/v1/ad_profiles", {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify(data),
-    });
+  const userUid = await upsertUserInfo()
+  const data = await getSDAData(taxonomyRecords, userUid)
+  sendAdProfile(data)
+}
 
-    fetch(request);
+async function upsertUserInfo(): Promise<string> {
+  const userInfo = await getUserInfo()
+  const userUid = userInfo?.user_uid ? userInfo.user_uid : crypto.randomUUID()
+  if (!userInfo.user_uid) {
+    addUserInfo(userUid)
+  }
+  return userUid
+}
+
+function sendAdProfile(data: {
+  user_uid: string
+  sda_profile: {
+    user: { data: { name: string; ext: { segtax: number }; segment: { id: string }[] }[] }
+    content: { data: { name: string; ext: { segtax: number }; segment: { id: string }[] }[] }
+  }
+}) {
+  try {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    const request = new Request(config.backendUrl + 'api/v1/ad_profiles', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data)
+    })
+
+    fetch(request)
   } catch (e) {}
-};
+}
 
 async function checkEnabledExtension() {
-  const extensionData = await getDomainData("meeExtension");
-  return !extensionData || extensionData.enabled;
+  const extensionData = await getDomainData('meeExtension')
+  return !extensionData || extensionData.enabled
 }
 
 async function onCheckEnabledMessageHandled(
   message: Message,
   sendResponse: (response?: any) => void
 ) {
-  const enabledExtension = await checkEnabledExtension();
-  const disable_domains = await getDisableDomains();
-  const enabled =
-    message.url && !disable_domains.includes(message.url) && enabledExtension;
-  sendResponse({ enabled });
+  const enabledExtension = await checkEnabledExtension()
+  const disable_domains = await getDisableDomains()
+  const enabled = message.url && !disable_domains.includes(message.url) && enabledExtension
+  sendResponse({ enabled })
 }
 
 chrome.runtime.onInstalled.addListener(async function () {
-  await changeExtensionEnabled(import.meta.env.VITE_BROWSER === "safari");
-});
+  await changeExtensionEnabled(import.meta.env.VITE_BROWSER === 'safari')
+})
 
 interface Message {
-  msg: string;
-  data?: string | boolean | any;
-  url?: string;
-  domain?: string;
-  mode?: "enable" | "disable" | "remove";
+  msg: string
+  data?: string | boolean | any
+  url?: string
+  domain?: string
+  mode?: 'enable' | 'disable' | 'remove'
 }
 chrome.runtime.onMessage.addListener(
   (
@@ -115,39 +125,39 @@ chrome.runtime.onMessage.addListener(
     sendResponse: (response?: any) => void
   ) => {
     switch (message.msg) {
-      case "DOWNLOAD_WELLKNOWN": {
-        afterDownloadWellknown(message, sender);
-        return true;
+      case 'DOWNLOAD_WELLKNOWN': {
+        afterDownloadWellknown(message, sender)
+        return true
       }
-      case "UPDATE_ENABLED": {
-        changeExtensionEnabled(import.meta.env.VITE_BROWSER === "safari");
-        return true;
+      case 'UPDATE_ENABLED': {
+        changeExtensionEnabled(import.meta.env.VITE_BROWSER === 'safari')
+        return true
       }
-      case "UPDATE_SELECTOR": {
-        if (message.mode === "enable") {
-          enableNavigatorGPC();
+      case 'UPDATE_SELECTOR': {
+        if (message.mode === 'enable') {
+          enableNavigatorGPC()
         } else {
-          disableNavigatorGPC();
+          disableNavigatorGPC()
         }
         if (message.domain && message.mode) {
           toggleGPCHeaders(
             1,
             message.domain,
-            import.meta.env.VITE_BROWSER === "safari",
+            import.meta.env.VITE_BROWSER === 'safari',
             message.mode
-          );
+          )
         }
 
-        return true;
+        return true
       }
-      case "CONTENT_LOADED": {
-        onCheckEnabledMessageHandled(message, sendResponse);
-        onInitPage();
-        return true;
+      case 'CONTENT_LOADED': {
+        onCheckEnabledMessageHandled(message, sendResponse)
+        onInitPage()
+        return true
       }
       default: {
-        return false;
+        return false
       }
     }
   }
-);
+)
--- src/sda-profile.ts
+++ src/sda-profile.ts
@@ -1,102 +1,128 @@
-const getRandomId = (min: number, max: number) => {
-  return (Math.floor(Math.random() * (max - min + 1)) + min).toString();
-};
-const getAgeRangeID = () => {
-  return getRandomId(3, 15);
-};
-const getGenderID = () => {
-  return getRandomId(49, 52);
-};
-const getNonProfitID = () => {
-  return getRandomId(1539, 1544);
-};
+import * as use from '@tensorflow-models/universal-sentence-encoder'
 
-interface HistoryResult {
-  id: string;
-  count: number;
-  result: chrome.history.HistoryItem[];
+function getRandomId(min: number, max: number) {
+  return (Math.floor(Math.random() * (max - min + 1)) + min).toString()
+}
+function getAgeRangeID() {
+  return getRandomId(3, 15)
+}
+function getGenderID() {
+  return getRandomId(49, 52)
+}
+function getNonProfitID() {
+  return getRandomId(1539, 1544)
 }
 
-const getContentSegmentIds = async (taxonomyRecords: string[][]) => {
+interface HistoryResult {
+  id: string
+  count: number
+  result: chrome.history.HistoryItem[]
+}
+
+export async function getSDAData(taxonomyRecords: string[][], userUid: string) {
+  const contentSegment = await parseSearchHistory(taxonomyRecords)
+  return parseSDAJson(userUid, contentSegment)
+}
+
+async function parseSearchHistory(taxonomyRecords: string[][]) {
   const historyCount = await Promise.all(
     taxonomyRecords.map(async (record): Promise<HistoryResult> => {
-      const id = record[0];
-      const name = record[2];
-      const result = name ? await chrome.history.search({ text: name }) : [];
+      const id = record[0]
+      const name = record[2]
+      // currently regex for history
+      const model = await use.load()
+      const recordEmbedding = await model.embed(name)
+      recordEmbedding.print()
+      // const recordEmbed = recordEmbedding.arraySync()[0]
+      const history = await chrome.history.search({ text: '', maxResults: 1000 })
+      const historyStrings = history.map((i) => i.url).filter((i) => i) as string[]
+      const historyEmbeddings = await model.embed(historyStrings)
+      historyEmbeddings.print()
+      // historyEmbeddings.arraySync().forEach((historyEmbed) => {
+      //   const similarity = cosineSimilarity(recordEmbed, historyEmbed)
+      //   console.log(similarity)
+      // })
+
+      const result = name ? await chrome.history.search({ text: name }) : []
       var count = result.reduce((sum: number, item) => {
-        const visitCount = item.visitCount ?? 0;
-        return sum + visitCount;
-      }, 0);
+        const visitCount = item.visitCount ?? 0
+        return sum + visitCount
+      }, 0)
 
-      return { id: id, count: count, result: result };
+      return { id: id, count: count, result: result }
     })
-  );
+  )
 
-  const results = historyCount.filter((item) => item.count > 0);
+  const results = historyCount.filter((item) => item.count > 0)
 
-  const sorted = results.sort((item1, item2) =>
-    item1.count < item2.count ? 1 : -1
-  );
+  const sorted = results.sort((item1, item2) => (item1.count < item2.count ? 1 : -1))
 
   return sorted.map((item) => {
-    return { id: item.id };
-  });
-};
+    return { id: item.id }
+  })
+}
 
-export const getSDAData = async (
-  taxonomyRecords: string[][],
-  userUid: string
-) => {
-  const contentSegment = await getContentSegmentIds(taxonomyRecords);
+function cosineSimilarity(embeddingA, embeddingB) {
+  const dotProduct = embeddingA.dot(embeddingB).dataSync()
+  const normA = embeddingA.norm().dataSync()
+  const normB = embeddingB.norm().dataSync()
+  return dotProduct / (normA * normB)
+}
+
+function parseSDAJson(userUid: string, contentSegment: { id: string }[]) {
   return {
     user_uid: userUid,
     sda_profile: {
       user: {
         data: [
           {
-            name: "mee-browser-extension",
+            name: 'mee-browser-extension',
             ext: { segtax: 4 },
-            segment: [
-              { id: getAgeRangeID() },
-              { id: getGenderID() },
-              { id: getNonProfitID() },
-            ],
-          },
-        ],
+            segment: [{ id: getAgeRangeID() }, { id: getGenderID() }, { id: getNonProfitID() }]
+          }
+        ]
       },
       content: {
         data: [
           {
-            name: "mee-browser-extension",
+            name: 'mee-browser-extension',
             ext: { segtax: 6 },
-            segment: contentSegment,
-          },
-        ],
-      },
-    },
-  };
-};
+            segment: contentSegment
+          }
+        ]
+      }
+    }
+  }
+}
 
-const unquote = (str: string) => {
-  var match;
-  return ((match = str.match(/(['"]?)(.*)\1/)) && match[2]) || str;
-};
+function unquote(str: string) {
+  var match
+  return ((match = str.match(/(['"]?)(.*)\1/)) && match[2]) || str
+}
 
-const getValues = (line: string, sep: string): string[] => {
+function getValues(line: string, sep: string): string[] {
   return line.split(sep).map(function (value) {
-    return unquote(value);
-  });
-};
+    return unquote(value)
+  })
+}
 
-export const parseTaxonomyRecords = (tsv: string) => {
-  const sep = "\t";
-  const lines = tsv.split(/[\n\r]/);
-  if (lines.length < 2) return [];
+/**
+ * Parses the a TSV string containing taxonomy records, extracting the relevant
+ * data into an array of arrays.
+ *
+ * The function assumes that the first two lines of the input represent headers
+ * and keys, which are ignored. Each subsequent line is split into individual
+ * values based on the tab separator. Quoted values are unquoted during parsing.
+ */
+export function parseTaxonomyRecords(tsv: string) {
+  const sep = '\t'
+  const lines = tsv.split(/[\n\r]/)
+  if (lines.length < 2) return []
 
-  lines.splice(0, 2); //remove header and keys
+  lines.splice(0, 2) //remove header and keys
 
   return lines.reduce((output: string[][], line) => {
-    output.push(getValues(line, sep));
-    return output;
-  }, []);
-};
+    output.push(getValues(line, sep))
+    return output
+  }, [])
+}
